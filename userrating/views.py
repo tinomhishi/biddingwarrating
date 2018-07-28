d78618b9af00bf5e60ea4843f36139e5f3441b01
tinomhishi <39485512+tinomhishi@users.noreply.github.com>
2018-07-28 22:51:41 +0200

    Add files via upload

--- userrating/views.py
+++ userrating/views.py
@@ -2,13 +2,17 @@ import urllib.request
 import urllib.parse
 import urllib.error
 from datetime import datetime, timedelta
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, render_to_response
 from django.contrib.auth.decorators import login_required
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, reverse_lazy
 from django.contrib import messages
 from django.conf import settings
+from datetime import datetime, timedelta
+from django.http import HttpResponseRedirect
+from django.utils import timezone
 
 from .models import *
+from .forms import UserReviewForm
 
 
 @login_required
@@ -27,3 +31,71 @@ def purchases_list(request):
     purchases = ItemSale.objects.filter(buyer=request.user)
     return render(request, 'userrating/purchases.html',
                     {'purchases': purchases})
+
+@login_required
+def purchase_detail(request, pk):
+	purchase = ItemSale.objects.get(pk=pk)
+	
+	if request.method == 'POST':
+		form = UserReviewForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('userrating:purchase_detail', args=[purchase.pk]))
+		if edit_form.is_valid():
+			edit_form.save()
+			return HttpResponseRedirect(reverse('userrating:purchase_detail', args=[purchase.pk]))	
+	else:
+		data = {'itemsale': purchase , 'user': purchase.buyer}
+		form = UserReviewForm(initial=data)
+
+	try:
+		review = UserReview.objects.get(itemsale=purchase)
+	except:
+		review = None
+
+	edit_form = UserReviewForm(instance=review)
+
+	deadline =  purchase.created + timedelta(days=31)
+	
+	if deadline >= timezone.now():
+		show = True
+	else:
+		show = False	
+
+	return render(request, 'userrating/purchase_detail.html', {'purchase': purchase, 'form': form, 'show': show, 'review': review, 'edit_form': edit_form})
+
+
+@login_required
+def sale_detail(request, pk):
+	sale = ItemSale.objects.get(pk=pk)
+	start_date = sale.created
+	deadline =  start_date + timedelta(days=31)
+	
+	try:
+		review = UserReview.objects.get(itemsale=sale)
+	except:
+		review = None
+
+	edit_form = UserReviewForm(instance=review)
+	
+	if deadline >= start_date:
+		show = True
+	else:
+		show = False	
+
+	if request.method == 'POST':
+		form = UserReviewForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('userrating:sale_detail', args=[sale.pk]))
+		if edit_form.is_valid():
+			edit_form.save()
+			return HttpResponseRedirect(reverse('userrating:sale_detail', args=[sale.pk]))
+				
+	else:
+		data = {'itemsale': sale , 'user': sale.seller}
+		form = UserReviewForm(initial=data)
+
+	return render(request, 'userrating/sale_detail.html', {'sale': sale, 'form': form, 'show': show, 'review': review, 'edit_form': edit_form})	
+
+
